a835bced043e69906fa52a9026e60103f6c7a848
Evan Verma <edverma@icloud.com>
2024-12-01 18:46:19 -0500

    add comments to help with future days

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,14 @@ mod utils;
 fn main() {
     println!("Advent of Code 2024 Answers:\n");
     days::day01::solve();
+
+    //    days::day02::solve();
+    //    days::day03::solve();
+    //    days::day04::solve();
+    //    days::day05::solve();
+    //    days::day06::solve();
+    //    days::day07::solve();
+    //    days::day08::solve();
+    //    days::day09::solve();
+    //    days::day10::solve();
 }
